1b472a4fff941b2b9c618ee80b699ece78ebee60
adama0911 <goudiabyadama0911@gmail.com>
2018-05-09 08:23:37 +0000

    adama-09-08h23

--- src/app/accueiladminpdv/accueiladminpdv.component.ts
+++ src/app/accueiladminpdv/accueiladminpdv.component.ts
@@ -658,7 +658,7 @@ private closeModal(): void {
   this.getRegionNewCaissier();
 
   this.errorConfirm = false;
-  this.closeBtn.nativeElement.click();
+  //this.closeBtn.nativeElement.click();
   console.log('close');
 }
 
@@ -802,7 +802,7 @@ validerdmde(){
       this.ibanExcessif = true ;
       return 1 ;
     }
-    this._adminpdvService.demandeRetrait({montant:this.montant.toString()}).subscribe(
+    this._adminpdvService.demandeRetrait({montant:this.montant}).subscribe(
       data => this.montant = undefined,
       error => alert(error),
       () => this.loading = false
@@ -892,19 +892,18 @@ public validerdeposite(){
   console.log("-1-------------------------------")
   if(confirm("Confirmer la demande")){
     console.log("je confirme")
-    this._adminpdvService.validerDemandeDepot({montant: this.montantdeposit, infocc: JSON.stringify(this.agentcc).toString(), infocom: this.modeversement!=undefined?this.modeversement:""})
+    this._adminpdvService.validerDemandeDepot({montant: this.montantdeposit, infocc: JSON.stringify(this.agentcc), infocom: this.modeversement!=undefined?this.modeversement:""})
       .subscribe(
         data => {
           console.log("---------------------------------------");
           console.log(data);
         },
         error => alert(error),
-        () => this.hidedepositeModal()
+        () => this.monitoringModal.hide()
       );
   } else{
     console.log("Je ne confirme pas")
   }
-
 }
 
 public geoloc:{latitude:number, longitude:number} = JSON.parse(localStorage.getItem("coordonneesgeospatiales")==null?localStorage.getItem("coordonneesgeospatiales"):JSON.stringify({latitude:0, longitude:0}));
@@ -1130,6 +1129,7 @@ public suivionepointIntervalleDashboard(){
   this._utilsService.getOnePointSuivicc({infoinit:'initdashboard', type:'intervalle', infotype:this.suivionepointSelectionintervalledateinit+" "+this.suivionepointSelectionintervalledatefinal})
     .subscribe(
       data => {
+
         console.log(data)
         this.id_gerant_selectionne = -1;
         if(data.errorCode){
@@ -1267,6 +1267,60 @@ public suivionepointSelectionGerant(indice: number){
   console.log("**************************************************************")
 }
 
+
+// Grophics
+
+  // Pie
+  public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartData:number[] = [300, 500, 100];
+  public pieChartType:string = 'pie';
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
+  public barChartOptions2:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels2:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType2:string = 'bar';
+  public barChartLegend2:boolean = true;
+ 
+  public barChartData2:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+ 
+  // events
+ 
+  public randomize():void {
+    // Only Change 3 values
+    let data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    let clone = JSON.parse(JSON.stringify(this.barChartData));
+    clone[0].data = data;
+    this.barChartData = clone;
+    /**
+     * (My guess), for Angular to recognize the change in the dataset
+     * it has to change the dataset variable directly,
+     * so one way around it, is to clone the data, change it and then
+     * assign it;
+     */
+  }
 
 /* ***************************************************************************
                      exploitation
